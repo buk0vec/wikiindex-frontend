af0e90020db410ff948c83f3825e3032ed9593b5
Nick Bukovec <nbukovec@calpoly.edu>
2024-06-25 13:06:31 -0700

    disclaimer

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-24 bg-pine-glade-100">
       <h1 className="text-6xl font-extrabold font-serif">WikiIndex</h1>
       <p>A research demo to visualize how retrieval augmented generation (RAG) applications answer questions</p>
+      <p>Note: this doesn&apos;t work anymore since I stopped paying for the cloud resources to run it. Sorry!</p>
       <ButtonLink href="/dashboard">Get started</ButtonLink>
       <p>Created by <a href="https://bukovec.dev" target="_blank" rel="noopener noreferrer" className="underline underline-offset-2 hover:opacity-80">Nick Bukovec</a></p>
     </main>
